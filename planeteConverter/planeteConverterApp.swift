f4d21f453af187391c5c14e62daaec6eda53b661
Benedikt Gottstein <benedikt@godstone.ch>
2024-04-16 10:47:19 +0200

    Initial Commit

---
+++ planeteConverter/planeteConverterApp.swift
@@ -0,0 +1,32 @@
+//
+//  planeteConverterApp.swift
+//  planeteConverter
+//
+//  Created by Benedikt Gottstein on 16.04.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct planeteConverterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
